85194f29f413dfdacac5623c8e8bb3ef89e64a49
paulo.camuri <40446143+camurri@users.noreply.github.com>
2024-12-28 20:51:51 -0300

    Atualizar o dowhile.dart
    Correção do comments

--- dowhile.dart
+++ dowhile.dart
@@ -2,12 +2,12 @@ void main() {
   int i = 0;
 
   while (i > 10) {
-    //Verifica a condição antes de executar o bloco
+    //Verifica a condição depois de executar o bloco
     print('Olá');
   }
 
   do {
-    print('Olá 2'); //Verifica a condição depois de executar o bloco
+    print('Olá 2'); //Verifica a condição antes de executar o bloco
     i++;
   } while (i > 10);
 }
